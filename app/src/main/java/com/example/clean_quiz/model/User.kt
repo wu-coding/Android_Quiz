a5f78d6aeffb95cdc4fb5a72c321a875b6785558
atfwu_laptop <8242698+wu-coding@users.noreply.github.com>
2022-03-19 00:55:44 -0400

    Switching computers

---
+++ app/src/main/java/com/example/clean_quiz/model/User.kt
@@ -0,0 +1,23 @@
+package com.example.clean_quiz.model
+
+class User {
+}
+
+class Score {
+
+}
+
+class choices {
+    enum class category {
+
+    }
+    enum class difficutly {
+
+    }
+    enum class category {
+
+    }
+    enum class tags {
+
+    }
+}
