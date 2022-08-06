f7ef2624264fe1bde262b95ea9aebdce625b859a
atfwu_laptop <8242698+wu-coding@users.noreply.github.com>
2022-08-06 07:30:32 -0400

    Remove Removed ApiKey

--- app/src/main/java/com/example/android_quiz/repository/QuizDataRepository.kt
+++ app/src/main/java/com/example/android_quiz/repository/QuizDataRepository.kt
@@ -1,6 +1,8 @@
 package com.example.android_quiz.repository
 
+import android.provider.Settings.Global.getString
 import androidx.sqlite.db.SimpleSQLiteQuery
+import com.example.android_quiz.BuildConfig
 import com.example.android_quiz.DAO.RecordDao
 import com.example.android_quiz.DAO.UserDao
 import com.example.android_quiz.data.User
@@ -8,6 +10,7 @@ import com.example.android_quiz.models.*
 import com.example.android_quiz.utils.addQueryParam
 import java.util.*
 import javax.inject.Inject
+import com.example.android_quiz.R
 
 class QuizDataRepository @Inject constructor(
     private val dataBase: Database,
@@ -115,7 +118,7 @@ class QuizDataRepository @Inject constructor(
             "tags" to userPrefData.category!!,
             "difficulty" to userPrefData.difficulty!!,
             "limit" to userPrefData.question_amount.toString(),
-            "apiKey" to "hcUZqLCh8uTaXt121DQd5IQ7wv5GFIVA5YlaPxy4"
+            "apiKey" to BuildConfig.APIKEY
         )
     }
 
